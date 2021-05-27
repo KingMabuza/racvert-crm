b91bfbe5cb67396ae12eb8c88918fc1a7bd2294e
KingMabuza <amal@amallevi.com>
2021-05-27 19:05:21 +0200

    Second Commit

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("db4free.net", "developer_crm", "", "racvert");
+$con=mysqli_connect("db4free.net", "developer_crm", "thisisatestpassword", "racvert");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
